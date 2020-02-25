879bf90c171bf253cae2291d80ed2c1aaa6866e5
ThisIsKaluwa <aseriousmail@aol.com>
2020-02-25 15:25:13 +0100

    Submit button is clickable without anything happening yet, design changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const main = function () {
     var max_fields = 5; //maximum input boxes allowed
     var wrapper = $(".input_fields_wrap"); //Fields wrapper
     var add_button = $(".add_field_button"); //Add button ID
+    var submit_button = $(".button");
     var x = 1; //initlal text box count
     add_button.click(function (e) { //on add input button click
         e.preventDefault();
@@ -11,8 +12,13 @@ const main = function () {
         }
     });
 
-    wrapper.on("click", ".remove_field", function (e) { //user click on remove text
+    wrapper.on("click", ".remove_field", function (e) { //user clicks on 'x'
         e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+
+    submit_button.click(function (f) {
+        f.preventDefault();
+        console.log("This button was pressed");
     })
 }
 window.addEventListener('DOMContentLoaded', main);
